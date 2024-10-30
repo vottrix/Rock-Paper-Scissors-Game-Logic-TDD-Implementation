4371d27ab3cfee17269faa1d57ad9ff496edb2fc
J.Mardini <abood.mardini@icloud.com>
2024-10-30 15:02:00 -0400

    testing push

---
+++ FailingTest/FailingTest.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace FailingTest
+{
+	TEST_CLASS(FailingTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			//TESt
+		}
+	};
+}
